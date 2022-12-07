76295814d985137f196870302d583e84d8f20bb7
vacheslavlapsin <Slava1001_93>
2022-12-07 10:15:29 +0300

    our project version 2

---
+++ src/main/java/com/example/Inst_Sait/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.Inst_Sait.payload.response;
+
+public class InvalidLoginResponse {
+
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse ()
+    {
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+
+    }
+}
